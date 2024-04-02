7741aa3000403f1469ae45a0e279ec3ead43a56e
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2024-04-02 09:22:21 +0700

    policy update

--- controller/managepolicy/managepolicy.go
+++ controller/managepolicy/managepolicy.go
@@ -1,8 +1,10 @@
 package managepolicy
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
@@ -12,202 +14,240 @@ import (
 )
 
 func IndexPolicy(w http.ResponseWriter, r *http.Request) {
-	// Dekode input JSON dari request body
-	var dashboardInput map[string]string
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&dashboardInput); err != nil {
-		response := map[string]interface{}{"message": err.Error(), "status": false}
-		helper.ResponseJSON(w, http.StatusBadRequest, response)
-		return
-	}
+    // Dekode input JSON dari request body
+    var dashboardInput map[string]string
+    decoder := json.NewDecoder(r.Body)
+    if err := decoder.Decode(&dashboardInput); err != nil {
+        response := map[string]interface{}{"message": err.Error(), "status": false}
+        helper.ResponseJSON(w, http.StatusBadRequest, response)
+        return
+    }
 
-	// Ambil nilai parameter dari input JSON
-	yearmonth := dashboardInput["yearmonth"]
-	app := dashboardInput["app"]
-	appChild := dashboardInput["app_child"]
+    // Ambil nilai parameter dari input JSON
+    yearmonth := dashboardInput["yearmonth"]
+    app := dashboardInput["app"]
+    appChild := dashboardInput["app_child"]
 
-	// Koneksi ke database
-	db := models.DBConnections[app]
-	fmt.Println(db)
-	if db == nil {
-		models.ConnectDatabase(app)
-		db = models.DBConnections[app]
-	}
-	fmt.Println(app)
-	// Set kolom yang akan diambil dari tabel
-	var columns string
-	switch app {
-	case "flexi":
-		columns = "policy_number, nomor_polis, kode_produk, kantor_cabang, no_rekening, premi, yearmonth, mulai_asuransi, selesai_asuransi, tanggal_lahir, url_sertifikat, risk, premi_refund, status, remark_refund, usia"
-	case "kpi", "afi":
-		columns = "policy_number, borrower, contract_number, submit_date, loan_start_date, loan_amount, rate, tenor, premium_amount, link_certificate, status, funding_partner"
-	case "adk":
-		columns = "policy_id, nomor_peminjaman, tanggal_awal_akad, tenor, tanggal_lahir, pokok_kredit, url_sertifikat, premium, rate, status, remark, yearmonth"
-	}
+    // Koneksi ke database
+    db := models.DBConnections[app]
+    fmt.Println(db)
+    if db == nil {
+        models.ConnectDatabase(app)
+        db = models.DBConnections[app]
+    }
+    fmt.Println(app)
 
-	// Ambil nilai parameter lainnya
-	length := 10
-	if lenStr := dashboardInput["length"]; lenStr != "" {
-		length, _ = strconv.Atoi(lenStr)
-	}
-	fmt.Println(length)
+    // Set kolom yang akan diambil dari tabel
+    var columns string
+    switch app {
+    case "flexi":
+        columns = "policy_number, nomor_polis, kode_produk, kantor_cabang, no_rekening, premi, yearmonth, mulai_asuransi, selesai_asuransi, tanggal_lahir, url_sertifikat, risk, premi_refund, status, remark_refund"
+    case "kpi", "afi":
+        columns = "policy_number, borrower, contract_number, submit_date, loan_start_date, loan_amount, rate, tenor, premium_amount, link_certificate, status, funding_partner"
+    case "adk":
+        columns = "policy_id, nomor_peminjaman, tanggal_awal_akad, tenor, tanggal_lahir, pokok_kredit, url_sertifikat, premium, rate, status, remark, yearmonth"
+    }
 
-	search := dashboardInput["search"]
-	contract := dashboardInput["contract"]
-	status := "All Status"
-	if statusStr := dashboardInput["status"]; statusStr != "" {
-		status = statusStr
-	}
+    // Ambil nilai parameter lainnya
+    length := 10
+    if lenStr := dashboardInput["length"]; lenStr != "" {
+        length, _ = strconv.Atoi(lenStr)
+    }
+    fmt.Println(length)
 
-	risk := "All Risk"
-	if riskStr := dashboardInput["risk"]; riskStr != "" {
-		risk = riskStr
-	}
+    search := dashboardInput["search"]
+    contract := dashboardInput["contract"]
+    status := "All Status"
+    if statusStr := dashboardInput["status"]; statusStr != "" {
+        status = statusStr
+    }
 
-	// Query untuk mendapatkan data dari database
-	var query string
-	if app == "kpi" || app == "afi" {
-		paramApp := app + "|" + appChild
-		if appChild == "All" {
-			paramApp = app
-		}
-		query = fmt.Sprintf("SELECT * FROM dashboard.sp_filter('admin', 'production|period', '%s');", paramApp)
-	} else {
-		query = "SELECT * FROM dashboard.sp_filter('admin', 'production|period');"
-	}
+    risk := "All Risk"
+    if riskStr := dashboardInput["risk"]; riskStr != "" {
+        risk = riskStr
+    }
 
-	// Eksekusi query
-	rows, err := db.Raw(query).Rows()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
+    // Query untuk mendapatkan data dari database
+    var query string
+    if app == "kpi" || app == "afi" {
+        paramApp := app + "|" + appChild
+        if appChild == "All" {
+            paramApp = app
+        }
+        query = fmt.Sprintf("SELECT * FROM dashboard.sp_filter('admin', 'production|period', '%s');", paramApp)
+    } else {
+        query = "SELECT * FROM dashboard.sp_filter('admin', 'production|period');"
+    }
 
-	// Ambil periode dan labelnya
-	var periods []models.Period
-	selectedPeriod := ""
+    // Eksekusi query
+    rows, err := db.Raw(query).Rows()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
 
-	// Iterasi setiap baris hasil query
-	for rows.Next() {
-		var yearMonth, label string
-		// Pindai nilai kolom ke dalam variabel struktur
-		if err := rows.Scan(&yearMonth, &label); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+    // Ambil periode dan labelnya
+    var periods []models.Period
+    // selectedPeriod := ""
 
-		period := models.Period{
-			YearMonth: yearMonth,
-			Label:     label,
-		}
+    // Iterasi setiap baris hasil query
+    for rows.Next() {
+        var yearMonth, label string
+        // Pindai nilai kolom ke dalam variabel struktur
+        if err := rows.Scan(&yearMonth, &label); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
 
-		periods = append(periods, period)
-	}
+        period := models.Period{
+            YearMonth: yearMonth,
+            Label:     label,
+        }
 
-	// Set parameter periode default jika tidak disediakan
-	if yearmonth == "" && len(periods) > 0 {
-		yearmonth = periods[len(periods)-1].YearMonth
-	}
+        periods = append(periods, period)
+    }
 
-	// Query untuk mendapatkan data policy dari database
-	query = "SELECT " + columns + " FROM dashboard.policy WHERE "
-	if search != "" && app != "flexi" && app != "afi" && app != "kpi" && app != "adk" {
-		query += fmt.Sprintf("nomor_aplikasi_pk = '%s' AND ", search)
-	}
+    // Set parameter periode default jika tidak disediakan
+    if yearmonth == "" && len(periods) > 0 {
+        yearmonth = periods[len(periods)-1].YearMonth
+    }
 
-	if search != "" && app == "flexi" {
-		query += fmt.Sprintf("no_rekening = '%s' AND ", search)
-	}
+    // Query untuk mendapatkan data policy dari database
+    query = "SELECT " + columns + " FROM dashboard.policy WHERE "
+    if search != "" && app != "flexi" && app != "afi" && app != "kpi" && app != "adk" {
+        query += fmt.Sprintf("nomor_aplikasi_pk = '%s' AND ", search)
+    }
 
-	if search != "" && (app == "kpi" || app == "afi") {
-		query += fmt.Sprintf("policy_number = '%s' AND ", search)
-	}
+    if search != "" && app == "flexi" {
+        query += fmt.Sprintf("no_rekening = '%s' AND ", search)
+    }
 
-	if search != "" && app == "adk" {
-		query += fmt.Sprintf("nomor_peminjaman = '%s' AND ", search)
-	}
+    if search != "" && (app == "kpi" || app == "afi") {
+        query += fmt.Sprintf("policy_number = '%s' AND ", search)
+    }
 
-	if contract != "" && (app == "kpi" || app == "afi") {
-		query += fmt.Sprintf("contract_number = '%s' AND ", contract)
-	}
+    if search != "" && app == "adk" {
+        query += fmt.Sprintf("nomor_peminjaman = '%s' AND ", search)
+    }
 
-	if status != "All Status" && app != "flexi" && app != "kpi" && app != "afi" {
-		query += fmt.Sprintf("status_policy = '%s' AND ", status)
-	}
+    if contract != "" && (app == "kpi" || app == "afi") {
+        query += fmt.Sprintf("contract_number = '%s' AND ", contract)
+    }
 
-	if risk != "All Risk" && app != "kpi" && app != "afi" {
-		query += fmt.Sprintf("risk = '%s' AND ", risk)
-	}
+    if status != "All Status" && app != "flexi" && app != "kpi" && app != "afi" {
+        query += fmt.Sprintf("status_policy = '%s' AND ", status)
+    }
 
-	if status != "All Status" && (app == "kpi" || app == "afi") {
-		query += fmt.Sprintf("status = '%s' AND ", status)
-	}
+    if risk != "All Risk" && app != "kpi" && app != "afi" {
+        query += fmt.Sprintf("risk = '%s' AND ", risk)
+    }
 
-	if appChild != "All" && (app == "kpi" || app == "afi") {
-		query += fmt.Sprintf("product = '%s' AND ", appChild)
-	}
+    if status != "All Status" && (app == "kpi" || app == "afi") {
+        query += fmt.Sprintf("status = '%s' AND ", status)
+    }
 
-	if yearmonth != "" {
-		query += fmt.Sprintf("yearmonth = '%s' AND ", yearmonth)
-	}
+    if appChild != "All" && (app == "kpi" || app == "afi") {
+        query += fmt.Sprintf("product = '%s' AND ", appChild)
+    }
 
-	if app == "kpi" {
-		query += "funding_partner = 'PT Bank Jago Tbk' AND "
-	}
+    if yearmonth != "" {
+        query += fmt.Sprintf("yearmonth = '%s' AND ", yearmonth)
+    }
 
-	if app == "afi" {
-		query += "funding_partner = 'PT ATOME FINANCE INDONESIA' AND "
-	}
+    if app == "kpi" {
+        query += "funding_partner = 'PT Bank Jago Tbk' AND "
+    }
 
-	// Hapus "AND" terakhir dari query
-	query = strings.TrimSuffix(query, "AND ")
+    if app == "afi" {
+        query += "funding_partner = 'PT ATOME FINANCE INDONESIA' AND "
+    }
 
-	// Eksekusi query
-	rows, err = db.Raw(query).Rows()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
+    // Hapus "AND" terakhir dari query
+    query = strings.TrimSuffix(query, "AND ")
+    fmt.Println(query)
 
-	var policies []models.PolicyData
+    // Eksekusi query
+    rows, err = db.Raw(query).Rows()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
 
-	// Iterasi setiap baris hasil query
-for rows.Next() {
-	var policy models.PolicyData
-	// Pindai nilai kolom ke dalam variabel struktur
-	switch app {
-	case "flexi":
-		if err := rows.Scan(&policy.PolicyNumber, &policy.NomorPolis, &policy.KodeProduk, &policy.KantorCabang, &policy.NoRekening, &policy.Premi, &policy.YearMonth, &policy.MulaiAsuransi, &policy.SelesaiAsuransi, &policy.TanggalLahir, &policy.URLSertifikat, &policy.Risk, &policy.PremiRefund, &policy.Status, &policy.RemarkRefund, &policy.Usia); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	case "kpi", "afi":
-		if err := rows.Scan(&policy.PolicyNumber, &policy.Borrower, &policy.ContractNumber, &policy.SubmitDate, &policy.LoanStartDate, &policy.LoanAmount, &policy.Rate, &policy.Tenor, &policy.PremiumAmount, &policy.LinkCertificate, &policy.Status, &policy.FundingPartner); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	case "adk":
-		if err := rows.Scan(&policy.PolicyID, &policy.NomorPeminjaman, &policy.TanggalAwalAkad, &policy.Tenor, &policy.TanggalLahir, &policy.PokokKredit, &policy.URLSertifikat, &policy.Premium, &policy.Rate, &policy.Status, &policy.Remark, &policy.YearMonth); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	}
+    var policies []models.PolicyData
 
-	policies = append(policies, policy)
-}
+    // Iterasi setiap baris hasil query
+    for rows.Next() {
+        var policy models.PolicyData
+        // Pindai nilai kolom ke dalam variabel struktur
+        switch app {
+        case "flexi":
+            var premiRefund, remarkRefund sql.NullString
+            if err := rows.Scan(&policy.PolicyNumber, &policy.NomorPolis, &policy.KodeProduk, &policy.KantorCabang, &policy.NoRekening, &policy.Premi, &policy.YearMonth, &policy.MulaiAsuransi, &policy.SelesaiAsuransi, &policy.TanggalLahir, &policy.URLSertifikat, &policy.Risk, &premiRefund, &policy.Status, &remarkRefund); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+            if premiRefund.Valid {
+                policy.PremiRefund = premiRefund.String
+            } else {
+                policy.PremiRefund = "" 
+            }
+            if remarkRefund.Valid {
+                policy.RemarkRefund = remarkRefund.String
+            } else {
+                policy.RemarkRefund = "" 
+            }
+        case "kpi", "afi":
+            if err := rows.Scan(&policy.PolicyNumber, &policy.Borrower, &policy.ContractNumber, &policy.SubmitDate, &policy.LoanStartDate, &policy.LoanAmount, &policy.Rate, &policy.Tenor, &policy.PremiumAmount, &policy.LinkCertificate, &policy.Status, &policy.FundingPartner); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+        case "adk":
+            if err := rows.Scan(&policy.PolicyID, &policy.NomorPeminjaman, &policy.TanggalAwalAkad, &policy.Tenor, &policy.TanggalLahir, &policy.PokokKredit, &policy.URLSertifikat, &policy.Premium, &policy.Rate, &policy.Status, &policy.Remark, &policy.YearMonth); err != nil {
+                http.Error(w, err.Error(), http.StatusInternalServerError)
+                return
+            }
+        }
 
+        policies = append(policies, policy)
+    }
 
-	// Siapkan data untuk ditampilkan dalam format JSON
-	responseData := map[string]interface{}{
-		"data":            policies,
-		"periods":         periods,
-		"selected_period": selectedPeriod,
-		"status":          true,
-		"message":         "Berhasil mengambil data policy",
-	}
+    // Ambil parameter "length" untuk menentukan jumlah data yang ditampilkan per halaman
+    pageLength := 10
+    if lengthStr := dashboardInput["length"]; lengthStr != "" {
+        pageLength, _ = strconv.Atoi(lengthStr)
+    }
 
-	// Kirim respons JSON
-	helper.ResponseJSON(w, http.StatusOK, responseData)
+    // Bagi data ke dalam halaman-halaman
+    var paginatedPolicies []models.PolicyData
+    page := 1
+    startInd := (page - 1) * pageLength
+    endInd := startInd + pageLength
+    if len(policies) > startInd {
+        if len(policies) < endInd {
+            endInd = len(policies)
+        }
+        paginatedPolicies = policies[startInd:endInd]
+    }
+
+    // Siapkan data untuk ditampilkan dalam format JSON
+    responseData := map[string]interface{}{
+        "items":           paginatedPolicies,
+        "perPage":         pageLength,
+        "currentPage":     page,
+        "path":            r.URL.Path,
+        "query":           r.URL.Query(),
+        "fragment":        r.URL.Fragment,
+        "pageName":        "page",
+        "onEachSide":      3,
+        "options":         map[string]string{"path": r.URL.Path, "pageName": "page"},
+        "total":           len(policies),
+        "lastPage":        int(math.Ceil(float64(len(policies)) / float64(pageLength))),
+        "status":          true,
+        "message":         "Berhasil mengambil data policy",
+    }
+
+    // Kirim respons JSON
+    helper.ResponseJSON(w, http.StatusOK, responseData)
 }

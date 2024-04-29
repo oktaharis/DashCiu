fdc6ebed114180f79e2492355a70a1ed928d2aed
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2024-04-29 10:50:57 +0700

    melengkapi file di db

---
+++ flexi/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jeypc/homecontroller/controller/explorespl"
+	"github.com/jeypc/homecontroller/controller/filesredinesspl"
+	"github.com/jeypc/homecontroller/controller/manageclaimspl"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// Menggunakan variabel app dalam URL dan membiarkan page sebagai query parameter
+
+	r.HandleFunc("/explorespl", explore.ExploreSpl).Methods("POST")
+	r.HandleFunc("/filesredinesspl", filesredines.FilesSpl).Methods("POST")
+	r.HandleFunc("/manageclaimspl", manageclaim.ClaimSpl).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}

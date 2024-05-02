4a3fa34296eff76169d57514b02133cf0d600afb
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2024-05-02 11:30:18 +0700

    subro, poli, dash done

--- spl/main.go
+++ spl/main.go
@@ -5,19 +5,25 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/jeypc/homecontroller/controller/dashboardhandlerspl"
 	"github.com/jeypc/homecontroller/controller/explorespl"
 	"github.com/jeypc/homecontroller/controller/filesredinesspl"
 	"github.com/jeypc/homecontroller/controller/manageclaimspl"
+	"github.com/jeypc/homecontroller/controller/managepolicyspl"
+	"github.com/jeypc/homecontroller/controller/subrogationspl"
 )
 
 func main() {
 	r := mux.NewRouter()
 
 	// Menggunakan variabel app dalam URL dan membiarkan page sebagai query parameter
-
+	
+	r.HandleFunc("/dashboardspl", dashboardhandler.DashboardSpl).Methods("POST")
 	r.HandleFunc("/explorespl", explore.ExploreSpl).Methods("POST")
 	r.HandleFunc("/filesredinesspl", filesredines.FilesSpl).Methods("POST")
-	r.HandleFunc("/manageclaimspl", manageclaim.ClaimSpl).Methods("POST")
+	r.HandleFunc("/claimspl", manageclaim.ClaimSpl).Methods("POST")
+	r.HandleFunc("/policyspl", managepolicy.PolicySpl).Methods("POST")
+	r.HandleFunc("/subrogationspl", subrogation.SubrogationSpl).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ spl/models/managepolicy.go
@@ -0,0 +1,45 @@
+package models
+
+// ini adalah stuktur managepolicy
+type PolicyData struct {
+	PolicyNumber            string      `json:"policy_number"`
+	PackedCode              string      `json:"packed_code"`
+	Premium                 string      `json:"premium"`
+	StatusPolicy            interface{} `json:"status_policy"`
+	Nama                    string      `json:"nama"`
+	TanggalLahir            string      `json:"tanggal_lahir"`
+	TanggalMulai            string      `json:"tanggal_mulai"`
+	TanggalAkhir            string      `json:"tanggal_akhir"`
+	Usia                    string      `json:"usia"`
+	JmlBulanKredit          string      `json:"jml_bulan_kredit"`
+	HargaPertanggungan      string      `json:"harga_pertanggungan"`
+	TempatKerja                string      `json:"tempat_kerja"`
+	Kategori                string      `json:"kategori"`
+	NomorRekening           string      `json:"nomor_rekening"`
+	TanggalPerjanjianKredit string      `json:"tanggal_perjanjian_kredit"`
+	NoKtp                   string      `json:"no_ktp"`
+	NomorAplikasiPK         string      `json:"nomor_aplikasi_pk"`
+	Alamat                  string      `json:"alamat"`
+	CreatedAt               string      `json:"created_at"`
+	UpdatedAt               string      `json:"updated_at"`
+	Filename                string      `json:"filename"`
+	URLSertifikat           interface{} `json:"url_sertifikat"`
+	YearMonth               string      `json:"yearmonth"`
+	Risk                    bool        `json:"risk"`
+	ExpiredDate             string      `json:"expired_date"`
+}
+
+// type Paginator struct {
+// 	Items       []PolicyData           `json:"items"`
+// 	PerPage     int                    `json:"perPage"`
+// 	CurrentPage int                    `json:"currentPage"`
+// 	Path        string                 `json:"path"`
+// 	Query       map[string]string      `json:"query"`
+// 	Fragment    interface{}            `json:"fragment"`
+// 	PageName    string                 `json:"pageName"`
+// 	OnEachSide  int                    `json:"onEachSide"`
+// 	Options     map[string]interface{} `json:"options"`
+// 	Total       int                    `json:"total"`
+// 	LastPage    int                    `json:"lastPage"`
+// 	Periods     []Period               `json:"periods"`
+// }

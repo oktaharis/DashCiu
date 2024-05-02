91f2559b1a12b5f318db17e8d37b72fba733b175
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2024-05-02 15:12:03 +0700

    dashSPl & policySpl Done

--- spl/main.go
+++ spl/main.go
@@ -5,9 +5,11 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	dashboardhandler "github.com/jeypc/homecontroller/controller/dashboardspl"
 	"github.com/jeypc/homecontroller/controller/explorespl"
 	"github.com/jeypc/homecontroller/controller/filesredinesspl"
 	"github.com/jeypc/homecontroller/controller/manageclaimspl"
+	managepolicyspl "github.com/jeypc/homecontroller/controller/managepolicy"
 )
 
 func main() {
@@ -18,6 +20,8 @@ func main() {
 	r.HandleFunc("/explorespl", explore.ExploreSpl).Methods("POST")
 	r.HandleFunc("/filesredinesspl", filesredines.FilesSpl).Methods("POST")
 	r.HandleFunc("/manageclaimspl", manageclaim.ClaimSpl).Methods("POST")
+	r.HandleFunc("/managepolicyspl", managepolicyspl.PolicySpl).Methods("POST")
+	r.HandleFunc("/dashboardspl", dashboardhandler.IndexDashSpl).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ spl/models/subrogation.go
@@ -0,0 +1,34 @@
+package models
+
+import "time"
+
+// TableData merupakan struktur untuk menyimpan data tabel tunggal
+type TableData struct {
+    NomorAplikasiPk string      `json:"nomor_aplikasi_pk"`
+    BatchPolicy     int         `json:"batch_policy"`
+    Remark          *string     `json:"remark"` // Use pointer for nullable field
+    BatchClaim      int         `json:"batch_claim"`
+    NilaiClaim      string      `json:"nilai_claim"`
+    NilaiSubrogasi string      `json:"nilai_subrogasi"`
+    SisaTerTagih    string      `json:"sisa_tertagih"`
+    Paid            string      `json:"paid"`
+    Status          string      `json:"status"`
+    CreatedAt       time.Time   `json:"created_at"`
+}
+
+
+// Paginator merupakan struktur untuk menyimpan data paginasi
+type Paginator struct {
+	Items       []TableData            `json:"items"`
+	PerPage     int                    `json:"perPage"`
+	CurrentPage int                    `json:"currentPage"`
+	Path        string                 `json:"path"`
+	Query       map[string]string      `json:"query"`
+	Fragment    interface{}            `json:"fragment"`
+	PageName    string                 `json:"pageName"`
+	OnEachSide  int                    `json:"onEachSide"`
+	Options     map[string]interface{} `json:"options"`
+	Total       int                    `json:"total"`
+	LastPage    int                    `json:"lastPage"`
+	Periods     []Period               `json:"periods"`
+}

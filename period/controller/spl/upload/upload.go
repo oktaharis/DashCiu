627e6492d6ccaed91fcad3fee5b6b07d0b83481e
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2024-05-08 16:03:50 +0700

    upload period selesai spl

--- period/controller/spl/upload/upload.go
+++ period/controller/spl/upload/upload.go
@@ -9,21 +9,17 @@ import (
 	"github.com/jeypc/homecontroller/models"
 )
 func UploadPeriodSpl(w http.ResponseWriter, r *http.Request) {
-	// Parse tahun dan bulan dari query parameter "yearmonth"
 	yearMonth := r.URL.Query().Get("yearmonth")
 
-	// Jika query parameter kosong, gunakan tahun dan bulan saat ini
+	// Jika yearMonth kosong, beri nilai default "202312"
 	if yearMonth == "" {
-		yearMonth = time.Now().Format("200601")
+		yearMonth = "202312"
 	}
 
-	// Variabel untuk menyimpan hasil query
 	var results []models.Period
 
-	// Parse tahun dan bulan dari format "YYYYMM"
 	parsedTime, err := time.Parse("200601", yearMonth)
 	if err != nil {
-		// Menangani kesalahan
 		log.Println("Error parsing date:", err)
 		helper.ResponseJSON(w, http.StatusInternalServerError, map[string]interface{}{
 			"status":  false,
@@ -32,16 +28,17 @@ func UploadPeriodSpl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Format tanggal menjadi "Aug 2021"
-	label := parsedTime.Format("Jan 2006")
+	for i := 0; i < 13; i++ {
+		newMonth := parsedTime.AddDate(0, i, 0)
+		yearMonth := newMonth.Format("200601")
+		label := newMonth.Format("Jan 2006")
+		results = append(results, models.Period{
+			YearMonth: yearMonth,
+			Label:     label,
+			Selected:  false,
+		})
+	}
 
-	// Tambahkan hasil ke slice results
-	results = append(results, models.Period{
-		YearMonth: yearMonth,
-		Label:     label,
-	})
-
-	// Kirim respons JSON
 	helper.ResponseJSON(w, http.StatusOK, map[string]interface{}{
 		"data":    results,
 		"status":  true,

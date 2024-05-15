3c95de34e1cc7c7f6af4775642bf8ec35b1893ac
AtefAnggaMalikFadilah <atefanggamf@gmail.com>
2024-05-15 09:55:39 +0700

    add user afi

--- afi/main.go
+++ afi/main.go
@@ -23,16 +23,16 @@ func main() {
 
 	// Menggunakan variabel app dalam URL dan membiarkan page sebagai query parameter
 	models.ConnectDatabase("afi")
-	r.HandleFunc("/dashboard", dashboardafi.IndexDashAfi).Methods("GET")
-	r.HandleFunc("/managepolicy", managepolicyafi.IndexPolicyAfi).Methods("GET")
-	r.HandleFunc("/manageclaim", manageclaimafi.IndexClaim).Methods("GET")
-	r.HandleFunc("/fileredines", filereadinessafi.IndexFilesAfi).Methods("GET")
-	r.HandleFunc("/user", userafi.UserAfi).Methods("GET")
+	r.HandleFunc("/dashboardafi", dashboardafi.IndexDashAfi).Methods("GET")
+	r.HandleFunc("/managepolicyafi", managepolicyafi.IndexPolicyAfi).Methods("GET")
+	r.HandleFunc("/manageclaimafi", manageclaimafi.IndexClaim).Methods("GET")
+	r.HandleFunc("/fileredinesafi", filereadinessafi.IndexFilesAfi).Methods("GET")
+	r.HandleFunc("/userafi", userafi.UserAfi).Methods("GET")
 
 	// ini period
-	r.HandleFunc("/claim/period", claimperiodafi.ClaimPeriodAfi).Methods("GET")
-	r.HandleFunc("/dashboard/period", dashboardperiodafi.DashboardPeriodAfi).Methods("GET")
-	r.HandleFunc("/managepolicy/period", policyperiodafi.PolicyPeriodAfi).Methods("GET")
+	r.HandleFunc("/claimafi/period", claimperiodafi.ClaimPeriodAfi).Methods("GET")
+	r.HandleFunc("/dashboardafi/period", dashboardperiodafi.DashboardPeriodAfi).Methods("GET")
+	r.HandleFunc("/managepolicyafi/period", policyperiodafi.PolicyPeriodAfi).Methods("GET")
 
 
 	log.Fatal(http.ListenAndServe(":8080", r))

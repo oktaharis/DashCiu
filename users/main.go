5d6faa14f926eb3dae383bbf18864546c2620166
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2024-05-17 09:14:58 +0700

    user_man monitor and user

--- users/main.go
+++ users/main.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"userscontroller/models"
+	monitoring "userscontroller/monitoringuser"
 	"userscontroller/users"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
-	models.ConnectDatabase("users")
+	models.ConnectDatabase()
 
 	// Menggunakan variabel app dalam URL dan membiarkan page sebagai query parameter
 	r.HandleFunc("/users", users.Users).Methods("GET")
+	r.HandleFunc("/monitoring", monitoring.IndexMonuser).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
